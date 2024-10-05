9d4d07c88a682f9131e8af70a3034da9e6d59e4e
Kapac_dc67@student.itstep.org <Kapac_dc67@student.itstep.org>
2024-10-05 14:07:42 +0300

    Add project files.

---
+++ Boiler/Boiler.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//struct Boiler {
+//    string brand, color;
+//    int power, volume;
+//    float temp;
+//};
+//
+//int main() {
+//    Boiler b;
+//
+//    cout << "Enter brand: ";
+//    getline(cin, b.brand);
+//
+//    cout << "Enter color: ";
+//    getline(cin, b.color);
+//
+//    cout << "Enter power (in watts): ";
+//    cin >> b.power;
+//
+//    cout << "Enter volume (in liters): ";
+//    cin >> b.volume;
+//
+//    cout << "Enter temperature (in °C): ";
+//    cin >> b.temp;
+//
+//    cout << "\nBoiler Information:\n";
+//    cout << "Brand: " << b.brand << endl;
+//    cout << "Color: " << b.color << endl;
+//    cout << "Power: " << b.power << " W" << endl;
+//    cout << "Volume: " << b.volume << " L" << endl;
+//    cout << "Temperature: " << b.temp << " °C" << endl;
+//
+//    return 0;
+//}
